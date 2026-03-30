d210fc1bbba876ab9efb8433cdbad86e4ef64e8b
YuukiKubozono <kubozonoyuuki1130@gmail.com>
2026-03-31 02:03:57 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (56.75%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        for x in set(ransomNote):
+            if ransomNote.count(x)>magazine.count(x):
+                return False
+
+        return True
