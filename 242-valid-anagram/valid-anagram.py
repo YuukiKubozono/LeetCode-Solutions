e546762d530c42353d7912dc79eb97893c7467b6
YuukiKubozono <kubozonoyuuki1130@gmail.com>
2026-04-06 18:34:39 +0900

    Time: 16 ms (23.22%) | Memory: 19.6 MB (35.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,18 +3,18 @@ class Solution:
         if len(s)!=len(t):
             return False
 
-        ds={}
-        dt={}
+        ds = {}
+        dt = {}
         for i in range(len(s)):
             if s[i] in ds:
-                ds[s[i]]+=1
+                ds[s[i]] += 1
             else:
-                ds[s[i]]=1
+                ds[s[i]] = 1
            
             if t[i] in dt:
-                dt[t[i]]+=1            
+                dt[t[i]] += 1            
             else:
-                dt[t[i]]=1  
+                dt[t[i]] = 1  
 
-        return ds==dt
+        return ds == dt
 
