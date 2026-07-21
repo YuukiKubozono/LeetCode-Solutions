7f6920a53d6f3ddf3775bea37695565a4b2e9c93
YuukiKubozono <kubozonoyuuki1130@gmail.com>
2026-07-21 16:22:02 +0900

    Time: 56 ms (5.44%) | Memory: 19.1 MB (90.82%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,16 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         num = n
-        k = []
-        for _ in range(32):
-            if num % 2 == 0:
-                k.append(0)
-            else:
-                k.append(1)
-            num = num // 2
-        
         ans = 0
-        for j in range(len(k)):
-            ans += k[j] * 2 ** (len(k) - 1 - j)
-
+        for _ in range(32):
+            i = n & 1
+            ans = (ans << 1) | i
+            n = n >> 1
         return ans
