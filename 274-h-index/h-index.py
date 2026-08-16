2f11ad0bf01ade05ae1400b5e3c2728f66a9bf41
YuukiKubozono <kubozonoyuuki1130@gmail.com>
2026-08-17 01:21:13 +0900

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.14%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,7 +1,6 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse = True)
-        ans = 0
         for i, c in enumerate(citations):
             if c < i + 1:
                 return i
