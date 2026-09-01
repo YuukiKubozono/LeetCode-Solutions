97c89249cc3b5a7bd9b8c41906c8173a6fb1a06e
YuukiKubozono <kubozonoyuuki1130@gmail.com>
2026-09-02 01:14:24 +0900

    Time: 3 ms (76.73%) | Memory: 19.2 MB (65.04%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,28 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        Roman_dict = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+
+        ans = []
+
+        for val, symbol in Roman_dict:
+            if num == 0:
+                break
+            while num >= val:
+                ans.append(symbol)
+                num -= val
+        
+        return "".join(ans)
