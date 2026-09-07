6003886b9eea1390ab012c155ef1f3c5679b4f69
久保薗友貴 <kubozonoyuuki1130@gmail.com>
2026-09-07 15:26:57 +0900

    Time: 7 ms (83.45%) | Memory: 19.4 MB (46.48%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -3,17 +3,16 @@ class Solution:
         if numRows >= len(s) or numRows == 1:
             return s
         
-        rows = []
-        for _ in range(numRows):
-            rows.append([])
+        rows = [""] * numRows
+        
         curr_row = 0
         direction = -1
 
         for char in s:
-            rows[curr_row].append(char)
+            rows[curr_row] += char
 
             if curr_row == 0 or curr_row == numRows - 1:
                 direction *= -1
             curr_row += direction
         
-        return "".join("".join(row) for row in rows)
+        return "".join(rows)
